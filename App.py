8788b4b345adabb0cba2ff1320aad3fa941bacfa
GulrukhRakhimova <gulrukh@iuj.ac.jp>
2024-10-31 13:03:27 +0900

    Update App.py

--- App.py
+++ App.py
@@ -12,7 +12,7 @@ import yfinance as yf
 st.set_page_config(layout="wide", page_title="WebApp_Demo")
 
 # Sidebar
-st.sidebar.title("Input Ticker")
+st.sidebar.title("Input")
 symbol = st.sidebar.text_input('Please enter the stock symbol: ', 'NVDA').upper()
 # Selection for a specific time frame.
 col1, col2 = st.sidebar.columns(2, gap="medium")
@@ -33,7 +33,6 @@ else:
 
 data = yf.download(symbol,start=sdate,end=edate)
 if data is not None:
-  st.write(data.describe())
   st.line_chart(data['Close'],x_label="Date",y_label="Close")
 else:
     st.error("Failed to fetch historical data.")
